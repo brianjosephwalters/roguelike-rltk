fc0268f9c54b06381938a995cd0d807ad2ed91f6
Brian J. Walters <brian.joseph.walters@gmail.com>
2020-02-27 17:52:43 -0600

    Completed 1.2 added map and movement of player with walls.

--- src/main.rs
+++ src/main.rs
@@ -21,29 +21,12 @@ struct Renderable {
 #[derive(Component, Debug)]
 struct Player {}
 
-#[derive(Component)]
-struct LeftMover {}
-
-struct LeftWalker {}
-
-impl<'a> System<'a> for LeftWalker {
-    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
-    fn run(&mut self, (left, mut pos): Self::SystemData) {
-        for (_lefty, pos) in (&left, &mut pos).join() {
-            pos.x -= 1;
-            if pos.x < 0 { pos.x = 79; }
-        }
-    }
-}
-
 struct State {
     ecs: World,
 }
 
 impl State {
     fn run_systems(&mut self) {
-        let mut lw = LeftWalker{};
-        lw.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
@@ -53,8 +36,10 @@ impl GameState for State {
         ctx.cls();
 
         player_input(self, ctx);
-
         self.run_systems();
+
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -68,10 +53,14 @@ impl GameState for State {
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
 
     for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79, max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
+        let destination_index = xy_index(pos.x + delta_x, pos.y + delta_y);
+        if map[destination_index] != TileType::Wall {
+            pos.x = min(79, max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));    
+        }
     }
 }
 
@@ -84,6 +73,64 @@ fn player_input(gs: &mut State, ctx: &mut Rltk) {
             VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
             VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
             _ => {}
+        }
+    }
+}
+
+#[derive(PartialEq, Copy, Clone)]
+enum TileType {
+    Wall, Floor
+}
+
+pub fn xy_index(x: i32, y: i32) -> usize {
+    (y as usize * 80) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80*50];
+
+    for x in 0..80 {
+        map[xy_index(x, 0)] = TileType::Wall;
+        map[xy_index(x, 49)] = TileType::Wall;
+    }
+
+    for y in 0..50 {
+        map[xy_index(0, y)] = TileType::Wall;
+        map[xy_index(79, y)] = TileType::Wall;
+    }
+
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    for _i in 0..400 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let index = xy_index(x, y);
+        if index != xy_index(40, 25) {
+            map[index] = TileType::Wall;
+        }
+    }
+
+    map
+}
+
+fn draw_map(map: &[TileType], ctx: &mut Rltk) {
+    let mut y = 0;
+    let mut x = 0;
+
+    for tile in map.iter() {
+        match tile {
+            TileType::Floor => {
+                ctx.set(x, y, RGB::from_f32(0.5, 0.5, 0.5), RGB::from_f32(0., 0., 0.), rltk::to_cp437('.'));
+            }
+            TileType::Wall => {
+                ctx.set(x, y, RGB::from_f32(0.0, 1.0, 0.0), RGB::from_f32(0., 0., 0.), rltk::to_cp437('#'));
+            }
+        }
+
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
         }
     }
 }
@@ -101,8 +148,9 @@ fn main() {
 
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
-    gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
+
+    gs.ecs.insert(new_map());
 
     gs.ecs
     .create_entity()
@@ -114,19 +162,6 @@ fn main() {
     })
     .with(Player{})
     .build();
-
-    for i in 0..10 {
-        gs.ecs
-        .create_entity()
-        .with(Position { x: i * 7, y: 20} )
-        .with(Renderable {
-            glyph: rltk::to_cp437('☺'),
-            fg: RGB::named(rltk::RED),
-            bg: RGB::named(rltk::BLACK),
-        })
-        .with(LeftMover {})
-        .build();
-    }
 
     rltk::main_loop(context, gs);
 }

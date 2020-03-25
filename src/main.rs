cea56bfb6b5e86a0398ebd98beb040c5c4a311e0
Brian J. Walters <brian.joseph.walters@gmail.com>
2020-03-24 21:01:34 -0500

    Finished 1.4: added FOV with 'seen' map.

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,9 @@ pub use player::*;
 mod rect;
 pub use rect::*;
 
+mod visibility_system;
+use visibility_system::VisibilitySystem;
+
 
 pub struct State {
     pub ecs: World,
@@ -23,6 +26,8 @@ pub struct State {
 
 impl State {
     fn run_systems(&mut self) {
+        let mut vis = VisibilitySystem{};
+        vis.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
@@ -34,8 +39,7 @@ impl GameState for State {
         player_input(self, ctx);
         self.run_systems();
 
-        let map = self.ecs.fetch::<Vec<TileType>>();
-        draw_map(&map, ctx);
+        draw_map(&self.ecs, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -60,10 +64,11 @@ fn main() {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
+    gs.ecs.register::<Viewshed>();
 
-    let (rooms, map) = new_map_rooms_and_corridors();
+    let map: Map = Map::new_map_rooms_and_corridors();
+    let (player_x, player_y) = map.rooms[0].center();
     gs.ecs.insert(map);
-    let (player_x, player_y) = rooms[0].center();
 
     gs.ecs
     .create_entity()
@@ -74,6 +79,7 @@ fn main() {
         bg: RGB::named(rltk::BLACK),
     })
     .with(Player{})
+    .with(Viewshed{ visible_tiles: Vec::new(), range: 8, dirty: true})
     .build();
 
     rltk::main_loop(context, gs);
